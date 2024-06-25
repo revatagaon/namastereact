46822684484d803033ab2091c9ae6e76cf5c86cd
prevatagaon <prevatagaon@appstec-me.com>
2024-06-25 20:53:18 +0530

    Ignited the App

--- 02-ignite/app.js
+++ 02-ignite/app.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+// React Core method for creating a React element
 const heading = React.createElement(
   "h1",
   { id: "heading" },
@@ -9,5 +10,11 @@ const heading = React.createElement(
 
 console.log(heading);
 
+// Crating a React Element using JSX
+
+const jsxHeading = <h1 id="heading">Namaste React using JSX 🚀</h1>;
+
+console.log(jsxHeading);
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(jsxHeading);
